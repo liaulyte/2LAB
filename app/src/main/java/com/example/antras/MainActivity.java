ce439d7e20ff6861a260bad33fa46fc40ae9b907
LauraSub <laura.subaciut@gmail.com>
2022-03-13 19:50:21 +0200

    initial commit

---
+++ app/src/main/java/com/example/antras/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.antras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    Spinner spOptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tvResult = findViewById(R.id.tvResult);
+        this.spOptions = findViewById(R.id.spOtions);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.counting_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spOptions.setAdapter(adapter);
+
+    }
+
+    public void onBtnCalculateClick(View view) {
+    }
+}
